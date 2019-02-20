57d4259ac4ee0a901f4c50bd21297a342224b129
eskarpnes <erl1sharpy@gmail.com>
2019-02-20 22:50:58 +0100

    Made some placeholder quizzes

---
+++ Munin/assets/quizzes.js
@@ -0,0 +1,76 @@
+export const quizzes = [
+  {
+    "id": 0,
+    "longitude":10.38536,
+    "latitude":63.42889,
+    "questions": [
+      {
+        "q": "Når ble Gunnerus biblioteket opprettet?",
+        "a": [
+          "1768",
+          "1868",
+          "1968",
+          "1905"
+        ]
+      },
+      {
+        "q": "Hvem er biblioteket oppkalt etter?",
+        "a": [
+          "Johan Ernst Gunnerus",
+          "Jonathan Gunnerus",
+          "Jakob Edvin Gunnerus",
+          "Jeger Bø Gunnerus"
+        ]
+      }
+    ]
+  },
+  {
+    "id": 1,
+    "longitude":10.395896,
+    "latitude":63.426904,
+    "questions": [
+      {
+        "q": "Når ble Gunnerus biblioteket opprettet?",
+        "a": [
+          "1768",
+          "1868",
+          "1968",
+          "1905"
+        ]
+      },
+      {
+        "q": "Hvem er biblioteket oppkalt etter?",
+        "a": [
+          "Johan Ernst Gunnerus",
+          "Jonathan Gunnerus",
+          "Jakob Edvin Gunnerus",
+          "Jeger Bø Gunnerus"
+        ]
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "longitude":10.401903,
+    "latitude":63.419610,
+    "questions": [
+      {
+        "q": "Når ble Gunnerus biblioteket opprettet?",
+        "a": [
+          "1768",
+          "1868",
+          "1968",
+          "1905"
+        ]
+      },
+      {
+        "q": "Hvem er biblioteket oppkalt etter?",
+        "a": [
+          "Johan Ernst Gunnerus",
+          "Jonathan Gunnerus",
+          "Jakob Edvin Gunnerus",
+          "Jeger Bø Gunnerus"
+        ]
+      }
+    ]
+  }]
