09f995cc3065d8e19eb930524f9fbc011827e64e
Christian Bernard Bouwhuis Røkke <christian.b.rokke@gmail.com>
2019-02-20 14:45:52 +0100

    Added checking responses and getting result

--- Munin/components/Question.js
+++ Munin/components/Question.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, Alert} from 'react-native';
 
 export default class Question extends Component{
     constructor(props) {
@@ -7,18 +7,37 @@ export default class Question extends Component{
 
     }
 
-
-
-
-    componentDidMount(){
-
+    checkAnswer (a) {
+        if (a == this.props.question.a[0]){
+            Alert.alert(
+                "Result",
+                "A WINNER IS YOU",
+                [{text: 'OK', onPress: () => console.log('OK pressed')}]
+            )
+        } else {
+            Alert.alert(
+                "Result",
+                "YOU SUCK",
+                [{text: 'OK', onPress: () => console.log('OK pressed')}]
+            )
+        }
     }
+
 
     render(){
         return(
             <View>
-                <Text>Question {this.props.counter}:  {this.props.question.q}</Text>
+                <View>
+                    <Text>Question {this.props.counter}:  {this.props.question.q}</Text>
+                </View>
+                <View>
+                    <Button title={this.props.question.a[0]} onPress={()=>this.checkAnswer(this.props.question.a[0])}></Button>
+                    <Button title={this.props.question.a[1]} onPress={()=>this.checkAnswer(this.props.question.a[1])}></Button>
+                    <Button title={this.props.question.a[2]} onPress={()=>this.checkAnswer(this.props.question.a[2])}></Button>
+                    <Button title={this.props.question.a[3]} onPress={()=>this.checkAnswer(this.props.question.a[3])}></Button>
+                </View>
             </View>
+
         )
     }
 
