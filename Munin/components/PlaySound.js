7e9770a2a4ecf82bdb0a70d56de94a53974c62ee
frodevan <fvdm1992@yahoo.no>
2019-03-13 14:49:58 +0100

    lagt til lydeffekter, abstraksjonslag og kode til å starte å spille async lyd

---
+++ Munin/components/PlaySound.js
@@ -0,0 +1,33 @@
+import { Audio } from 'expo';
+
+
+export const Sounds = {
+    doiing: require('../assets/sounds/hello.wav'),
+    click: require('../assets/sounds/klikk.wav'),
+    gong: require('../assets/sounds/gong.wav'),
+    shortFart: require('../assets/sounds/fart1.wav'),
+    longFart: require('../assets/sounds/fart2.wav'),
+    fanfare: require('../assets/sounds/tada.wav'),
+    clock: require('../assets/sounds/8sec.wav'),
+}
+
+export const Music = {
+    elevator: require('../assets/sounds/VisionOnGalleryTheme.mp3'),
+    adventure: require('../assets/sounds/AdventureLine.mp3'),
+}
+
+//
+// Usage ex:
+//      PlaySound(Sounds.doiing);
+//
+export const PlaySound = async (snd) =>
+{
+    const SoundObject = new Audio.Sound();
+    try
+    {
+        await SoundObject.loadAsync(snd);
+        await SoundObject.playAsync();
+    }
+    catch (error)
+    {}
+}
