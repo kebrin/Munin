0e73d76f4a048c8d869482908e346f6911db81d0
Tollef Jørgensen <tollefj@gmail.com>
2019-03-13 09:41:04 +0100

    updated user screen

--- Munin/screens/UserScreen.js
+++ Munin/screens/UserScreen.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units'
-import Modal from 'react-native-modal'
-import {
-  Image,
-  ImageBackground,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  TouchableHighlight
-} from 'react-native';
+import { vw, vh } from 'react-native-expo-viewport-units'
+// import Modal from 'react-native-modal'
+import { Button, SwipeModal, ModalContent } from '../components'
+
+import { ImageBackground, View } from 'react-native';
 
 const ProfileImg = ({size}) => (
   <ImageBackground
@@ -29,78 +21,6 @@ const ProfileImg = ({size}) => (
     }}
   />
 )
-
-const PositionText = ({top, text}) => (
-  <Text style={{
-    top: top ? 0 : 'auto',
-    bottom: top ? 'auto': 0,
-    position: 'absolute',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }}>{text}</Text>
-)
-
-const CustomModal = ({show, dismiss, innercomponent}) => (
-  <Modal children={innercomponent}
-    isVisible={show}
-    animationIn="slideInUp"
-    animationOut="zoomOutUp"
-    animationInTiming={400}
-    animationOutTiming={1000}
-    transparent={true}
-    backdropColor='black'
-    backdropOpacity={.5}
-    backdropTransitionInTiming={1000}
-    backdropTransitionOutTiming={500}
-    onBackdropPress={dismiss}
-    onBackButtonPress={dismiss}
-    onSwipe={dismiss}
-    swipeDirection="up"
-    >
-  </Modal>
-)
-
-const ModalContent = ({title}) => (
-  <View style={{
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'white',
-    height: 500
-  }}>
-    <PositionText top={true} text={title} />
-    <PositionText top={false} text='Swipe up to close 🙃☝' />
-  </View>
-)
-
-const Button = ({title, color, handleChange}) => (
-  <TouchableOpacity style={{
-    width: vw(60),
-    height: 70,
-    backgroundColor: color,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10
-  }}
-    onPress={() => handleChange(true)}
-  >
-    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-      {title}
-    </Text>
-  </TouchableOpacity>
-)
-
-const BigText = ({text}) => (
-  <Text style={{
-    bottom: 0,
-    position: 'absolute',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }}>
-    {text}
-  </Text>
-)
-
 
 export default class UserScreen extends React.Component {
   state = {
@@ -126,7 +46,6 @@ export default class UserScreen extends React.Component {
   dismissQuiz = () => { this.setState({ quizzesClicked: false })}
   toggleQuiz = () => { this.setState({ quizzesClicked: !this.state.quizzesClicked})}
 
-
   render() {
     return (
       <View style={{
@@ -137,11 +56,11 @@ export default class UserScreen extends React.Component {
         flexDirection: 'column'
       }}>
         <ProfileImg size={vw(70)} />
-        <CustomModal
+        <SwipeModal
           show={this.state.resultsClicked}
           dismiss={this.dismissResults}
           innercomponent={<ModalContent title='Results!'/>}/>
-        <CustomModal
+        <SwipeModal
           show={this.state.quizzesClicked}
           dismiss={this.dismissQuiz}
           innercomponent={<ModalContent title='Quizzer i nærheten!'/>}/>
