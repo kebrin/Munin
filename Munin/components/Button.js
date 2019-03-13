df640c697866d5d8823e83bf6415c413168fcdd7
Tollef Jørgensen <tollefj@gmail.com>
2019-03-13 09:40:53 +0100

    structure components

---
+++ Munin/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TouchableOpacity, Text} from 'react-native'
+
+export const Button = ({title, color, handleChange}) => (
+  <TouchableOpacity style={{
+    width: vw(60),
+    height: 70,
+    backgroundColor: color,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10
+  }}
+    onPress={() => handleChange(true)}
+  >
+    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+)
