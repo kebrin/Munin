7936de246d424a8eb86c4d29ce234c8c8b66062e
Christian Bernard Bouwhuis Røkke <christian.b.rokke@gmail.com>
2019-03-13 14:14:27 +0100

    New quizzes

--- Munin/assets/quizzes.js
+++ Munin/assets/quizzes.js
@@ -1,50 +1,94 @@
 export const quizzes = [
     {
         "id": 0,
-        "longitude":10.38536,
+        "longitude":10.38536, //endre til Hellas?
         "latitude":63.42889,
         "questions": [
             {
-                "q": "Når ble Gunnerus biblioteket opprettet?",
+                "q": "Delos used to be known as \"Adelos\", because:",
                 "a": [
-                    "1768",
-                    "1868",
-                    "1968",
-                    "1905"
+                    "It floated around the Aegean obscured by waves",
+                    "There was no mention of it in ancient texts",
+                    "It had not been officially catalogued as an island"
                 ]
             },
             {
-                "q": "Hvem er biblioteket oppkalt etter?",
+                "q": "It was forbidden:",
                 "a": [
-                    "Johan Ernst Gunnerus",
-                    "Jonathan Gunnerus",
-                    "Jakob Edvin Gunnerus",
-                    "Jeger Bø Gunnerus"
+                    "To moor your ship at Delos, without Apollo's priests' permission",
+                    "To spend the night on Delos",
+                    "To be born or to die on Delos"
+                ]
+            }
+            {
+                "q": "Apollo and Artemis were the children of:",
+                "a": [
+                    "Zeus and the titan maiden Leto",
+                    "Poseidon and Nefertiti",
+                    "Zeus and Hera"
+                ]
+            }
+            {
+                "q": "The symbol of Apollo was:",
+                "a": [
+                    "The Lyre and Bow",
+                    "The thunder",
+                    "The Myrtle tree"
+                ]
+            }
+            {
+                "q": "Delos was destroyed in 88BC:",
+                "a": [
+                    "By Mithridates and the Athenians",
+                    "By the Romans",
+                    "By the Spartans"
                 ]
             }
         ]
     },
     {
         "id": 1,
-        "longitude":10.395896,
+        "longitude":10.395896, //endre til Irak?
         "latitude":63.426904,
         "questions": [
             {
-                "q": "Når ble Gunnerus biblioteket opprettet?",
+                "q": "Assyria, was a",
                 "a": [
-                    "1768",
-                    "1868",
-                    "1968",
-                    "1905"
+                    "Mesopotamian East Semitic kingdom and an empire of the Ancient Near East",
+                    "Mesopotamian East Semitic kingdom",
+                    "Empire of the Ancient Near Eas."
                 ]
             },
             {
-                "q": "Hvem er biblioteket oppkalt etter?",
+                "q": "Choose the king that was Assyrian in descent?",
                 "a": [
-                    "Johan Ernst Gunnerus",
-                    "Jonathan Gunnerus",
-                    "Jakob Edvin Gunnerus",
-                    "Jeger Bø Gunnerus"
+                    "Salmanassar",
+                    "Tutankhamon",
+                    "Salgon"
+                ]
+            }
+            {
+                "q": "Niniveh was the capital of:",
+                "a": [
+                    "The Assyrian empire",
+                    "The Persian empire",
+                    "Akkadian empire"
+                ]
+            }
+            {
+                "q": "Cuneiform script",
+                "a": [
+                    "is one of the earliest systems of writing",
+                    "is the idiom the Assyrians spoke",
+                    "is the accounting system of ancient Assyrians"
+                ]
+            }
+            {
+                "q": "March 2015 the Islamic state destroyed",
+                "a": [
+                    "The Assyrian cities Nineveh and Nimrud in Northern Irak",
+                    "Sculptures and statues from Mosul Museum",
+                    "Mosaics from ancient Palmyra"
                 ]
             }
         ]
